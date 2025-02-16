e10a8d884d2af945efc8108dfddd618c6f721432
gfloriano11 <gflorianodev@gmail.com>
2025-02-16 12:02:57 -0300

    feat: search js make fetch to catch data

--- src/public/scripts/search.js
+++ src/public/scripts/search.js
@@ -1,14 +1,28 @@
 const searchButton = document.querySelector('#search_icon');
 
 searchButton.addEventListener('click', () => {
+    
+    const prompt = document.querySelector('#search').value;
 
-    const loadText = document.createElement('p');
-
-    loadText.innerText = 'Buscando...';
-
-    const loadContainer = document.querySelector('#load_text');  
-
-    loadContainer.appendChild(loadText);
-
-    fetch('dev-challange/src/app/');
+    fetch('../app/?controller=search', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: prompt
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.success){
+            console.log(data.message);
+        } else {
+            console.log(data.message);
+        }
+    })
+    .catch(error => {
+        console.log('Requisiton Error: ', error)
+    })
+    
 })
