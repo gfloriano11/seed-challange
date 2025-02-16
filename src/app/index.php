7d35c02dc36196285ab598fda6fcad2b6b2cca29
gfloriano11 <gflorianodev@gmail.com>
2025-02-16 12:03:17 -0300

    feat: index php has files to application works

--- src/app/index.php
+++ src/app/index.php
@@ -1,3 +1,21 @@
 <?php
 
-    require_once ('');
+    require_once ('core/Core.php');
+
+    require_once ('controller/SearchController.php');
+
+    require_once ('model/search.php');
+
+    ob_start();
+
+        $core = new Core;
+
+        $core->start_app($_GET);
+
+        $result = ob_get_contents();
+
+    ob_end_clean();
+
+    echo $result;
+
+    // var_dump($_GET);
