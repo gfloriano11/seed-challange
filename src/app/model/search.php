b59de38796bc8d6ffe53153cabc282f255069fb1
gfloriano11 <gflorianodev@gmail.com>
2025-02-16 14:14:23 -0300

    feat: function to search

--- src/app/model/search.php
+++ src/app/model/search.php
@@ -1,9 +1,21 @@
 <?php
 
-    class Search{
+class Search {
 
-        public static function search($prompt){
+    public static function search($prompt) {
 
+        $data['equipments'] = json_decode(file_get_contents('../../data/equipments.json'), true);
 
-        }
+        if ($data['equipments']) {
+            foreach ($data['equipments'] as $equipment) {
+                if (strpos(strtolower($equipment['EquipmentName']), strtolower($prompt)) !== false) {
+                    $result[] = $equipment; 
+                }
+            }
+        } 
+
+        return $result;
+
     }
+}
+?>
