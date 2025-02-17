ba61b6a76f1058380ee08c8d9b3a833f208cac70
gfloriano11 <gflorianodev@gmail.com>
2025-02-17 17:47:08 -0300

    feat: create function to pull data for the website

--- src/public/scripts/search.js
+++ src/public/scripts/search.js
@@ -1,6 +1,12 @@
 const searchButton = document.querySelector('#search_icon');
 
 searchButton.addEventListener('click', () => {
+
+    const products = document.querySelectorAll('.product');
+
+    products.forEach(product => {
+        product.remove();
+    })
     
     const prompt = document.querySelector('#search').value;
 
@@ -18,13 +24,30 @@ searchButton.addEventListener('click', () => {
         console.log(data)
         if(data.success){
             console.log(data.message);
-            
             data.equipments.forEach(equipment => {
                 let text = document.createElement('p');
                 text.innerText += equipment.EquipmentName
-                const body = document.querySelector('body');
+                const products_container = document.querySelector('.products');
+
+                const product = document.createElement('div');
+                const product_number = document.createElement('p')
+                const product_name = document.createElement('p');
+                const product_quantity = document.createElement('p');
+
+                products_container.classList.add('result');
+                product.classList.add('product')
+                product_number.classList.add('product_number');
+                product_name.classList.add('product_name');
+                product_quantity.classList.add('product_quantity');
+
+                product_number.innerText = '#' + equipment.EquipmentID;
+                product_name.innerText = equipment.EquipmentName;
+                product_quantity.innerText = 'Qtd: 200 pç'
     
-                body.appendChild(text);
+                products_container.appendChild(product);
+                product.appendChild(product_number);
+                product.appendChild(product_name);
+                product.appendChild(product_quantity);
             })
 
         } else {
