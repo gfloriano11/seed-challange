c25c46cd6cf1e341068b778008f6121def1d4431
gfloriano11 <gflorianodev@gmail.com>
2025-02-18 19:27:47 -0300

    feat: catch all json data

--- src/app/model/search.php
+++ src/app/model/search.php
@@ -5,11 +5,48 @@ class Search {
     public static function search($prompt) {
 
         $data['equipments'] = json_decode(file_get_contents('../../data/equipments.json'), true);
+        $data['materials'] = json_decode(file_get_contents('../../data/materials.json'), true);
+        $data['purchase_orders'] = json_decode(file_get_contents('../../data/purchase_orders.json'), true);
+        $data['sales_orders'] = json_decode(file_get_contents('../../data/sales_orders.json'), true);
+        $data['workforce'] = json_decode(file_get_contents('../../data/workforce.json'), true);
 
         if ($data['equipments']) {
             foreach ($data['equipments'] as $equipment) {
                 if (strpos(strtolower($equipment['EquipmentName']), strtolower($prompt)) !== false) {
                     $result[] = $equipment; 
+                }
+            }
+        }
+
+        if ($data['materials']) {
+            foreach ($data['materials'] as $equipment) {
+                if (strpos(strtolower($equipment['MaterialName']), strtolower($prompt)) !== false) {
+                    $result[] = $equipment; // 
+                }
+            }
+        }
+
+        if ($data['purchase_orders']) {
+            foreach ($data['purchase_orders'] as $equipment) {
+                if (strpos(strtolower($equipment['MaterialName']), strtolower($prompt)) !== false) {
+                    $result[] = $equipment; // pedido de compra
+                }
+            }
+        } 
+
+
+        if ($data['sales_orders']) {
+            foreach ($data['sales_orders'] as $equipment) {
+                if (strpos(strtolower($equipment['MaterialName']), strtolower($prompt)) !== false) {
+                    $result[] = $equipment; // pedido de compra
+                }
+            }
+        } 
+
+        if ($data['sales_orders']) {
+            foreach ($data['sales_orders'] as $equipment) {
+                if (strpos(strtolower($equipment['Name']), strtolower($prompt)) !== false) {
+                    $result[] = $equipment; // pedido de compra
                 }
             }
         } 
