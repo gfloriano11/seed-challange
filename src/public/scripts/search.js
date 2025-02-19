7435ed3b55f4401601a83ba64a16f9bb7690ddec
gfloriano11 <gflorianodev@gmail.com>
2025-02-18 21:08:27 -0300

    feat: verification to all json

--- src/public/scripts/search.js
+++ src/public/scripts/search.js
@@ -24,31 +24,77 @@ searchButton.addEventListener('click', () => {
         console.log(data)
         if(data.success){
             console.log(data.message);
-            data.equipments.forEach(equipment => {
-                let text = document.createElement('p');
-                text.innerText += equipment.EquipmentName
-                const products_container = document.querySelector('.products');
+            data.result.forEach(item => {
 
-                const product = document.createElement('div');
-                const product_number = document.createElement('p')
-                const product_name = document.createElement('p');
-                const product_quantity = document.createElement('p');
+                if(item){
 
-                products_container.classList.add('result');
-                product.classList.add('product')
-                product_number.classList.add('product_number');
-                product_name.classList.add('product_name');
-                product_quantity.classList.add('product_quantity');
+                    if(item.EquipmentID){
+                        
+                        let text = document.createElement('p');
+                        text.innerText += item.EquipmentName
+                        const products_container = document.querySelector('.products');
+        
+                        const product = document.createElement('div');
+                        const product_number = document.createElement('p')
+                        const product_name = document.createElement('p');
+                        const product_quantity = document.createElement('p');
+        
+                        products_container.classList.add('result');
+                        product.classList.add('product')
+                        product_number.classList.add('product_number');
+                        product_name.classList.add('product_name');
+                        product_quantity.classList.add('product_quantity');
+        
+                        product_number.innerText = '#' + item.EquipmentID;
+                        product_name.innerText = item.EquipmentName;
+                        product_quantity.innerText = 'Qtd: 200 pç'
+            
+                        products_container.appendChild(product);
+                        product.appendChild(product_number);
+                        product.appendChild(product_name);
+                        product.appendChild(product_quantity);
 
-                product_number.innerText = '#' + equipment.EquipmentID;
-                product_name.innerText = equipment.EquipmentName;
-                product_quantity.innerText = 'Qtd: 200 pç'
-    
-                products_container.appendChild(product);
-                product.appendChild(product_number);
-                product.appendChild(product_name);
-                product.appendChild(product_quantity);
-            })
+                    }
+
+                    if(item.MaterialID){
+                        ;
+                        text.innerText += item.EquipmentName
+                        const products_container = document.querySelector('.products');
+        
+                        const product = document.createElement('div');
+                        const product_number = document.createElement('p')
+                        const product_name = document.createElement('p');
+                        const product_quantity = document.createElement('p');
+        
+                        products_container.classList.add('result');
+                        product.classList.add('product')
+                        product_number.classList.add('product_number');
+                        product_name.classList.add('product_name');
+                        product_quantity.classList.add('product_quantity');
+        
+                        product_number.innerText = '#' + item.EquipmentID;
+                        product_name.innerText = item.EquipmentName;
+                        product_quantity.innerText = 'Qtd: 200 pç'
+            
+                        products_container.appendChild(product);
+                        product.appendChild(product_number);
+                        product.appendChild(product_name);
+                        product.appendChild(product_quantity);
+                    }
+
+                    if(item.PurchaseOrderID){
+
+                    }
+
+                    if(item.SalesOrderID){
+                        
+                    }
+
+                    if(item.WorkforceID){
+
+                    }
+                }
+            });
 
         } else {
             console.log(data.message);
